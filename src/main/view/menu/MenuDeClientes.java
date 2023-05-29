6b0c77025436e2b7f815196d52e7ddc455f890b7
Lucas Cardoso de Carvalho <adslucascardoso@gmail.com>
2023-05-29 14:41:00 -0300

    update

--- src/main/view/menu/MenuDeClientes.java
+++ src/main/view/menu/MenuDeClientes.java
@@ -1,5 +1,53 @@
 package main.view.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+import main.util.*;
+import main.view.*;
+
 public class MenuDeClientes extends Menu {
     
+    private List<ItemMenu> itens = new ArrayList<>();
+
+    public MenuDeClientes(){
+
+        adicionar(1, Message.CREATE, new Command(){
+            public void exe(){
+                //CHAMA METODOS DO CONTROLLER 
+            }
+        });
+
+        adicionar(2, Message.READ, new Command(){
+            public void exe(){
+                //CHAMA METODOS DO CONTROLLER 
+            }
+        });
+
+        adicionar(3, Message.UPDATE, new Command(){
+            public void exe(){
+                //CHAMA METODOS DO CONTROLLER 
+            }
+        });
+
+        adicionar(4, Message.DELETE, new Command(){
+            public void exe(){
+                //CHAMA METODOS DO CONTROLLER 
+            }
+        });
+
+        adicionar(5, Message.VOLTAR, new Command(){
+            public void exe(){
+                new MainView().show(); // Chama a View Principal
+            }
+        });
+        
+    }
+
+    public void adicionar(Integer index, String texto, Command command) {
+		itens.add(new ItemMenu(index, texto, command));
+	}
+
+	public List<ItemMenu> getItens() {
+		return itens;
+	}
 }
